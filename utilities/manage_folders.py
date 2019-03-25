ef5fb96c7b4c40b0475674db2a5512c09a470da1
GSilva1 <gaston_silva@mcafee.com>
2019-03-25 15:50:05 -0300

    Adding __main__.py and logic to crete and delete test folder

--- utilities/manage_folders.py
+++ utilities/manage_folders.py
@@ -1,10 +1,11 @@
 import os
 import shutil
 
-import ConfigParser
 
-
-def create_folders():
+def create_folder_base():
+    """This function have the main goal of create the folder that will be the base folder for this script.
+        In case that the base folder isn't empty, all the files/folder will be deleted
+    """
     base_folder = r"C:\_TEST_FOLDER"
 
     if os.path.exists(base_folder):
@@ -35,8 +36,32 @@ def delete_files(del_file_folder):
 
 
 def delete_folder(del_folder):
-    shutil.rmtree(del_folder)
+    try:
+        shutil.rmtree(del_folder)
+    except Exception as os_e:
+        print os_e
+
+
+def create_test_folder():
+
+    create_folder_base()
+
+    base_path = r"C:\_TEST_FOLDER"
+    if os.path.isdir(base_path):
+        for i in range(1, 11):
+            os.mkdir(base_path + r"\Folder" + str(i))
+
+
+def delete_test_folder():
+
+    if os.listdir(r"C:\_TEST_FOLDER"):
+        try:
+            shutil.rmtree(r"C:\_TEST_FOLDER")
+        except Exception as os_e:
+            print os_e
+    else:
+        print "Empty folder"
 
 
 if __name__ == "__main__":
-    create_folders()
+    create_test_folder()
